1c86013ba98cc849e71de4785637c7521a2e1350
machudat18 <datt@elcom.com.vn>
2022-01-12 14:56:25 +0700

    init

---
+++ android/app/src/main/kotlin/com/dat/google_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dat.google_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyAR6MC4k-M0GQVLU6muKhCPPGVv9AvFoiY';
+}
